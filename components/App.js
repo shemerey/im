b304912f78381eaa81646a2b999d69c10b36f091
Anton Shemerey <shemerey@gmail.com>
2016-08-17 12:29:12 +0300

    extract components

---
+++ components/App.js
@@ -0,0 +1,16 @@
+'use babel'
+
+import React, { Component } from 'react'
+
+// Components
+import SideBar from './SideBar'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="xxx">
+        <SideBar />
+      </div>
+    )
+  }
+}
---
+++ components/SideBar.js
@@ -0,0 +1,17 @@
+'use babel'
+
+import React, { Component } from 'react'
+
+export default class SideBar extends Component {
+  render() {
+    return (
+      <ul>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+        <li>1</li>
+      </ul>
+    )
+  }
+}
