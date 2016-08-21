9bf7e402953dfc72d79793b7c9d321e924a29741
Anton Shemerey <shemerey@gmail.com>
2016-08-22 00:09:12 +0300

    use editor properly

--- components/MasterInput.js
+++ components/MasterInput.js
@@ -1,18 +1,26 @@
 'use babel'
 
 import React, {PropTypes, Component} from 'react'
-import {TextEditor} from 'atom'
+import { TextEditor } from 'atom'
 
 export default class MasterInput extends Component {
   constructor(props) {
     super(props)
-    this.editor = React.createElement('atom-text-editor')
+    this.editor = TextEditor()
+  }
+
+  componentShouldUpdate() {
+    return false;
+  }
+
+  componentDidMount() {
+    this.refs.editor.appendChild(this.editor.getElement())
   }
 
   render() {
     return (
-      <div className="master-input">
-        {this.editor}
+      <div className="master-input" ref="editor">
+        {/* EditorElement here */}
       </div>
     )
   }
