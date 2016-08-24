17f42ca010ca19757646246fa7bd3dcc9028f7fb
Anton Shemerey <shemerey@gmail.com>
2016-08-24 13:42:20 +0300

    messages

--- components/Message.js
+++ components/Message.js
@@ -10,6 +10,28 @@ export default class Message extends Component {
     return (
       <div className="im-message">
         <div className="gutter">
+          <span className="username">
+            <a href="#">@{username}</a>
+          </span>
+        </div>
+        <div className="content">
+          <span>{text}</span>
+          {/* <a className="timestamp"><FormattedTime value={new Date(created_at)} format="hhmm" /></a> */}
+        </div>
+      </div>
+    )
+  }
+}
+
+/*
+
+export default class Message extends Component {
+  render() {
+    const { id, text, created_at, username} = this.props
+
+    return (
+      <div className="im-message">
+        <div className="gutter">
           <a className="timestamp"><FormattedTime value={new Date(created_at)} format="hhmm" /></a>
         </div>
         <div className="content">
@@ -22,3 +44,5 @@ export default class Message extends Component {
     )
   }
 }
+
+*/
