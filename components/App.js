51952328025f24cec203bcd3c2fbbdadeef9328f
Anton Shemerey <shemerey@gmail.com>
2016-09-02 02:27:31 +0300

    add loader

--- components/App.js
+++ components/App.js
@@ -3,19 +3,23 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-import { setCurrentTeam } from '../actions'
-
 import SideBar from './SideBar'
 import Main from './Main'
+import Loader from './Loader'
 
- class App extends Component {
-  render() {
+class App extends Component {
+  app() {
     return (
       <div className="im">
         <SideBar />
         <Main />
       </div>
     )
+  }
+
+  render() {
+    const appIsReady = true //false
+    return (appIsReady ? this.app() : <Loader />)
   }
 }
 
