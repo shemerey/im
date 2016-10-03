37f5a72f872896a230014925fffcc6f048cba828
Anton Shemerey <shemerey@gmail.com>
2016-10-03 10:50:44 +0300

    Add CurrentUserInfo

--- components/CurrentUserInfo.js
+++ components/CurrentUserInfo.js
@@ -1,14 +1,27 @@
 'use babel'
 
-import React, {PropTypes, Component} from 'react'
+import React, { PropTypes, Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class CurrentUserInfo extends Component {
+ class CurrentUserInfo extends Component {
   render() {
+    const { teamName, currentUser } = this.props
+
     return (
       <div className="user-info">
-        <div className="team-name">Atom</div>
-        <div className="user-name">@shemerey</div>
+        <div className="team-name">{ teamName }</div>
+        <div className="user-name">@{ currentUser.username }</div>
       </div>
     )
   }
 }
+
+function mapStateToProps(state) {
+  const currentUser = state.users[state.currentTeam.id].find((u) => u.id === state.currentTeam.userId)
+  return {
+    teamName: state.currentTeam.name,
+    currentUser
+  }
+}
+
+export default connect(mapStateToProps)(CurrentUserInfo)
