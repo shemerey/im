c9301daf480dc09aa5b24e778659f5f1e5854c2e
Anton Shemerey <shemerey@gmail.com>
2016-10-06 18:57:51 +0300

    Add users tests

--- spec/reducers-spec.js
+++ spec/reducers-spec.js
@@ -1,15 +1,33 @@
 'use babel';
 
 import TeamObject from '../lib/TeamObject'
+import UserObject from '../lib/UserObject'
+
+// Action
 import {
+  // Team
   addNewTeam,
   updateTeam,
   setCurrentTeam,
+  // User
+  setAllUsers,
+  addChannels,
+  addNewUser,
+  updateUser,
+  // Channel
+  setAllChannels,
+  setActiveChannel,
+  addNewChannel,
+  updateChannel,
+  // Messages
+  sendMessage,
 } from '../lib/actions'
 
+// Reducers
 import {
   teams,
   currentTeam,
+  users,
 } from '../lib/reducers'
 
 describe('Reducers', () => {
@@ -21,6 +39,19 @@ describe('Reducers', () => {
     new TeamObject({
       id: '222',
       name: 'second',
+    }),
+  ]
+
+  const [u1, u2] = [
+    new UserObject({
+      id: 'u1id',
+      teamId: 111,
+      username: 'firstUser',
+    }),
+    new UserObject({
+      id: 'u2id',
+      teamId: 111,
+      username: 'secondUser',
     }),
   ]
 
@@ -74,5 +105,124 @@ describe('Reducers', () => {
       expect(currentTeam(null, setCurrentTeam(t1))).toEqual(t1)
       expect(currentTeam(t1, setCurrentTeam(null))).toEqual(t1)
     });
+  })
+
+  describe('users', () => {
+    it('sets all users', () => {
+      expect(users({
+        xxx: {
+          u1id: u1,
+          u2id: u2,
+        },
+      }, setAllUsers({
+        teamId: 'xxx',
+        users: [u1],
+      }))).toEqual({
+        xxx: {
+          u1id: u1,
+        },
+      })
+
+      expect(users({
+        xxx: {
+          u1id: u1,
+          u2id: u2,
+        },
+      }, setAllUsers({
+        teamId: 'xxx',
+        users: [],
+      }))).toEqual({
+        xxx: {},
+      })
+
+      expect(users({}, setAllUsers({
+        teamId: 'xxx',
+        users: [u2],
+      }))).toEqual({
+        xxx: {
+          u2id: u2,
+        },
+      })
+
+      expect(users(undefined, setAllUsers({
+        teamId: 'xxx',
+        users: [u1, u2],
+      }))).toEqual({
+        xxx: {
+          u1id: u1,
+          u2id: u2,
+        },
+      })
+    })
+
+    it('Will not add new user if user already exists', () => {
+      expect(users({
+        [u1.teamId]: {
+          u1id: u1,
+        },
+      }, addNewUser(u1))).toEqual({
+        [u1.teamId]: {
+          u1id: u1,
+        },
+      })
+    })
+
+    it('Will add new user', () => {
+      expect(users(undefined, addNewUser(u1))).toEqual({
+        [u1.teamId]: {
+          u1id: u1,
+        },
+      })
+
+      expect(users({}, addNewUser(u1))).toEqual({
+        [u1.teamId]: {
+          u1id: u1,
+        },
+      })
+
+      expect(users({
+        [u1.teamId]: {
+          u1id: u1,
+        },
+      }, addNewUser(u2))).toEqual({
+        [u1.teamId]: {
+          u1id: u1,
+          u2id: u2,
+        },
+      })
+    })
+
+    it('add new user when addNewUser action fiered', () => {
+      expect(users({
+        xxx: {
+          u1id: u1,
+          u2id: u2,
+        },
+      }, setAllUsers({
+        teamId: 'xxx',
+        users: [],
+      }))).toEqual({
+        xxx: {},
+      })
+
+      expect(users({}, setAllUsers({
+        teamId: 'xxx',
+        users: [u2],
+      }))).toEqual({
+        xxx: {
+          u2id: u2,
+        },
+      })
+
+      expect(users(undefined, setAllUsers({
+        teamId: 'xxx',
+        users: [u1, u2],
+      }))).toEqual({
+        xxx: {
+          u1id: u1,
+          u2id: u2,
+        },
+      })
+    })
   })
 })
