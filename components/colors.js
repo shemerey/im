b808f89f140936f3df5b04cf764db00c51b207e0
Anton Shemerey <shemerey@gmail.com>
2016-10-14 15:47:58 +0300

    add colors

--- components/colors.js
+++ components/colors.js
@@ -1,7 +1,95 @@
 'use babel'
 
-const background = 'red'
+const appBackground = '#21252b'
+const baseBackground = '#282c34'
+const background = baseBackground
+const text = '#9da5b4'
+const textSubtle = 'rgba(157, 165, 180, 0.6)'
+const textHighlight = '#d7dae0'
+const textSelected = '#ffffff'
+const textInfo = '#6494ed'
+const textSuccess = '#73c990'
+const textWarning = '#e2c08d'
+const textError = '#ff6347'
+const bgInfo = '#0088ff'
+const bgSuccess = '#20b684'
+const bgWarning = '#cc8533'
+const bgError = '#d92626'
+const bgHighlight = '#31363f'
+const bgSelected = '#3a3f4b'
+const baseBorder = '#181a1f'
+const paneItemBg = '#282c34'
+const paneItemBorder = '#181a1f'
+const inputBg = '#1b1d23'
+const inputBorder = '#181a1f'
+const toolPanelBg = '#21252b'
+const toolPanelBorder = '#181a1f'
+const insetPanelBg = '#31363f'
+const panelHeadingBg = '#282c34'
+const panelHeadingBorder = '#181a1f'
+const overlayBg = '#282c34'
+const overlayBorder = '#181a1f'
+const buttonBg = '#353b45'
+const buttonBgHover = '#3a3f4b'
+const buttonBgSelected = '#528bff'
+const buttonBorder = '#181a1f'
+const tabBarBg = '#21252b'
+const tabBarBorder = '#181a1f'
+const tabBg = '#21252b'
+const tabBgActive = '#282c34'
+const tabBorder = '#181a1f'
+const treeViewBg = '#21252b'
+const treeViewBorder = '#181a1f'
+const uiSiteColor1 = '#0088ff'
+const uiSiteColor2 = '#20b684'
+const uiSiteColor3 = '#cc8533'
+const uiSiteColor4 = '#d831b0'
+const uiSiteColor5 = '#ebdd5b'
 
 export default {
+  appBackground,
+  baseBackground,
   background,
+  text,
+  textSubtle,
+  textHighlight,
+  textSelected,
+  textInfo,
+  textSuccess,
+  textWarning,
+  textError,
+  bgInfo,
+  bgSuccess,
+  bgWarning,
+  bgError,
+  bgHighlight,
+  bgSelected,
+  baseBorder,
+  paneItemBg,
+  paneItemBorder,
+  inputBg,
+  inputBorder,
+  toolPanelBg,
+  toolPanelBorder,
+  insetPanelBg,
+  panelHeadingBg,
+  panelHeadingBorder,
+  overlayBg,
+  overlayBorder,
+  buttonBg,
+  buttonBgHover,
+  buttonBgSelected,
+  buttonBorder,
+  tabBarBg,
+  tabBarBorder,
+  tabBg,
+  tabBgActive,
+  tabBorder,
+  treeViewBg,
+  treeViewBorder,
+  uiSiteColor1,
+  uiSiteColor2,
+  uiSiteColor3,
+  uiSiteColor4,
+  uiSiteColor5,
 }
