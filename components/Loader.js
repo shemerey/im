97f71058e763d2d401de5a142e3e95d121cf4530
Anton Shemerey <shemerey@gmail.com>
2016-10-14 11:49:49 +0300

    extract loader css and transform it to pure fuction

--- components/Loader.js
+++ components/Loader.js
@@ -1,23 +1,62 @@
 'use babel'
 
-import React, { PropTypes, Component } from 'react'
+import React from 'react'
+import styled, { keyframes } from 'styled-components'
 
-export default class Loader extends Component {
-  render() {
-    return (
-      <div className="im-full-screen-container">
-        <div className="sk-cube-grid">
-          <div className="sk-cube sk-cube1"></div>
-          <div className="sk-cube sk-cube2"></div>
-          <div className="sk-cube sk-cube3"></div>
-          <div className="sk-cube sk-cube4"></div>
-          <div className="sk-cube sk-cube5"></div>
-          <div className="sk-cube sk-cube6"></div>
-          <div className="sk-cube sk-cube7"></div>
-          <div className="sk-cube sk-cube8"></div>
-          <div className="sk-cube sk-cube9"></div>
-        </div>
-      </div>
-    )
+// keyframes animations
+const cubeGridScaleDelay = keyframes`
+    0%, 70%, 100% {
+      transform: scale3D(1, 1, 1);
+    } 35% {
+      transform: scale3D(0, 0, 1);
+    }
+`;
+
+// Loader with animations
+const CubeLoader = styled.div`
+  height: 100vh;
+
+  .sk-cube-grid {
+    width: 90px;
+    height: 90px;
+    position: fixed; /* or absolute */
+    top: 40%;
+    left: 45%;
   }
+
+  .sk-cube-grid .sk-cube {
+    width: 33%;
+    height: 33%;
+    background-color: white;
+    float: left;
+    animation: ${cubeGridScaleDelay} 1.3s infinite ease-in-out;
+  }
+
+  .sk-cube-grid .sk-cube1 { animation-delay: 0.2s; }
+  .sk-cube-grid .sk-cube2 { animation-delay: 0.3s; }
+  .sk-cube-grid .sk-cube3 { animation-delay: 0.4s; }
+  .sk-cube-grid .sk-cube4 { animation-delay: 0.1s; }
+  .sk-cube-grid .sk-cube5 { animation-delay: 0.2s; }
+  .sk-cube-grid .sk-cube6 { animation-delay: 0.3s; }
+  .sk-cube-grid .sk-cube7 { animation-delay: 0s; }
+  .sk-cube-grid .sk-cube8 { animation-delay: 0.1s; }
+  .sk-cube-grid .sk-cube9 { animation-delay: 0.2s; }
+`;
+
+export default function Loader() {
+  return (
+    <CubeLoader>
+      <div className="sk-cube-grid">
+        <div className="sk-cube sk-cube1" />
+        <div className="sk-cube sk-cube2" />
+        <div className="sk-cube sk-cube3" />
+        <div className="sk-cube sk-cube4" />
+        <div className="sk-cube sk-cube5" />
+        <div className="sk-cube sk-cube6" />
+        <div className="sk-cube sk-cube7" />
+        <div className="sk-cube sk-cube8" />
+        <div className="sk-cube sk-cube9" />
+      </div>
+    </CubeLoader>
+  )
 }
